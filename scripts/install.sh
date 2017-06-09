13326a1071efa9d60be88a9c0ca443fdc70e5919
torkleyy <thomas-schaller2000@gmx.de>
2017-06-10 00:09:11 +0200

    Fix hugo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 which mdbook || cargo install mdbook
-wget https://github.com/spf13/hugo/releases/download/v0.21/hugo_0.21_Linux-64bit.tar.gz && tar xvf hugo_0.21_Linux-64bit.tar.gz
-export PATH=$PATH:$(pwd)
+wget https://github.com/spf13/hugo/releases/download/v0.21/hugo_0.21_Linux-64bit.tar.gz &&
+tar xvf hugo_0.21_Linux-64bit.tar.gz &&
+cp hugo scripts/
 
